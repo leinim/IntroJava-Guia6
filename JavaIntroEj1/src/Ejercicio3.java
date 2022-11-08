3e37ad8f04c6893e207e241799b293af80505f2c
Milena Lingua <lingua.milena@gmail.com>
2022-11-08 20:22:40 -0300

    Ejercicio 3

---
+++ JavaIntroEj1/src/Ejercicio3.java
@@ -0,0 +1,31 @@
+/*
+Define variables donde puedas alojar los resultados y prueba 
+usar dos operadores de cada tipo.
+ */
+
+/**
+ *
+ * @author Mile
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //defino variables y uso operadores
+        int num1 = 4;
+        int num2 = 3;
+        
+        int suma = 3-4;
+        double resta = suma / 2;
+        boolean pregunta1 = suma >= resta;
+        boolean pregunta2 = num2 == suma;
+        num1--;
+        num2++;
+        
+        
+    }
+    
+}
