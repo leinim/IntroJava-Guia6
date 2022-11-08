65814f81696162faa36765701fe7e5853811b76d
Milena Lingua <lingua.milena@gmail.com>
2022-11-08 19:50:52 -0300

    Ejercicio 1

---
+++ JavaIntroEj1/src/Ejercicio1.java
@@ -0,0 +1,24 @@
+/*
+Crear un proyecto de Java y definir al menos 
+6 variables en tu IDE de distintos tipos de datos.
+ */
+
+/**
+ *
+ * @author Mile
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float flotante;
+        double doble;
+        boolean booleano;
+        char caracter;
+    
+    }
+    
+}
