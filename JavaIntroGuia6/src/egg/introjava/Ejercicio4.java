ee85722152be5325ba093cfe0c050838d85fc4d6
Milena Lingua <lingua.milena@gmail.com>
2022-11-08 21:13:17 -0300

    Ejercicio 4

---
+++ JavaIntroGuia6/src/egg/introjava/Ejercicio4.java
@@ -0,0 +1,27 @@
+/*
+Define una variable que aloje tu nombre y otra que guarde tu edad. 
+Imprime ambas variables por pantalla.
+ */
+package egg.introjava;
+
+/**
+ *
+ * @author Mile
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Defino las variables y les asigno valor
+        String nombre = "Milena";
+        int edad = 26;
+        // las imprimo de diferentes maneras
+        System.out.println("Mi nombre es " + nombre + ".");
+        System.out.print("Tengo ");
+        System.out.println(edad + " años.");
+        
+    }
+    
+}
