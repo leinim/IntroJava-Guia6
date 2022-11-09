69a2f87ba9dc87601082eaad962b361d72c02e51
Milena Lingua <lingua.milena@gmail.com>
2022-11-09 19:33:39 -0300

    Ejercicio 6

--- ManosALaObra/src/egg/introjava/manosalaobra/Ejercicio6.java
+++ ManosALaObra/src/egg/introjava/manosalaobra/Ejercicio6.java
@@ -4,6 +4,8 @@ determine si ambos o alguno de ellos es mayor a 25.
  */
 package egg.introjava.manosalaobra;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mile
@@ -15,6 +17,21 @@ public class Ejercicio6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int num1, num2;
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese dos numeros enteros");
+        num1 = leer.nextInt();
+        num2 = leer.nextInt();
+        
+        if (num1 > 25 && num2 > 25) {
+            System.out.println("Ambos números son mayores a 25");
+        } else if (num1 > 25 || num2 > 25) {
+            System.out.println("Uno de los números es mayor a 25");
+        } else {
+            System.out.println("Ninguno de los números es mayor a 25");
+        }
+        
     }
     
 }
