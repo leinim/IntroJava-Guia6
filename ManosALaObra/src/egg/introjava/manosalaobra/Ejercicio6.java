3c42c354b01276e400ef0df2cfc5761f41ca740b
Milena Lingua <lingua.milena@gmail.com>
2022-11-09 19:12:44 -0300

    Creacion carpeta ejercicio 1

---
+++ ManosALaObra/src/egg/introjava/manosalaobra/Ejercicio6.java
@@ -0,0 +1,20 @@
+/*
+Implementar un programa que le pida dos números enteros a usuario y 
+determine si ambos o alguno de ellos es mayor a 25.
+ */
+package egg.introjava.manosalaobra;
+
+/**
+ *
+ * @author Mile
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
